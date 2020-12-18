fb3b05f409797a62b4d64a8f3376f97d916e0305
saaketh <saakethaj@gmail.com>
2020-12-18 19:20:08 +0530

    about page done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./About.css";
 
 function About() {
-  return <div className="About">About</div>;
+  return (
+    <div className="About">
+      <div className="content">
+        <h3>About</h3>
+        <p>
+          Coder's Hospital pvt ltd,is an institution that is built, staffed, and
+          equipped for the diagnosis of disease; for the treatment, both medical
+          and surgical, of the sick and the injured; and for their housing
+          during this process. The modern hospital also often serves as a centre
+          for investigation and for teaching.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
