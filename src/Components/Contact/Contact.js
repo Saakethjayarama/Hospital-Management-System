d259a66a07d574f1dcccd6f9661c54e879b74c37
saaketh <saakethaj@gmail.com>
2020-12-18 19:18:03 +0530

    contact done

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,50 @@
 import React from "react";
 import "./Contact.css";
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
 
 function Contact() {
-  return <div className="Contact">Contact</div>;
+  const [state, setState] = useState({
+    name: "",
+  });
+
+  const handleChange = (event) => {
+    setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const { name } = state;
+    window.open(
+      `https://wa.me/917406254072?text=Hey i am ${name}, please lemme know about your hospital.`
+    );
+  };
+
+  console.log(state);
+  return (
+    <div className="Contact">
+      <div className="content">
+        <h3>Contact us</h3>
+        <Form>
+          <Form.Group controlId="name">
+            <Form.Control
+              type="text"
+              placeholder="Enter Name"
+              value={state.name}
+              onChange={handleChange}
+              name="name"
+            />
+          </Form.Group>
+
+          <Button variant="primary" onClick={handleClick}>
+            Drop Text
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
